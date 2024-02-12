e7937d1665567a91c4075b10c0579c1cb7033af1
Artur <ilchishinartur@gmail.com>
2024-02-12 12:02:06 +0300

    Added random numbers function.

--- src/components/PopupCalendar.jsx
+++ src/components/PopupCalendar.jsx
@@ -1,12 +1,28 @@
 import left_arrow from '../img/Arrow _ Left.png'
 import right_arrow from '../img/Arrow _ Right.png'
+import {useEffect} from "react";
 
 export const PopupCalendar = (props) => {
 
     const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT' ]
+
+    const generateDates = (firstDate, lastDate) => {
+        const randomDates = [];
+        for( let i = 0; i < 6; i++ ) {
+            const randomDate = Math.floor( firstDate + Math.random() * (lastDate + 1 - firstDate) )
+            console.log(randomDate)
+            randomDates.push(randomDate)
+        }
+        return () => {
+                return randomDates;
+        }
+    }
+
+    let inactiveDates = generateDates(1,31);
+    let soldOutDates = generateDates(1,31);
+    useEffect(() => {inactiveDates(); soldOutDates()}, [])
+
     const dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-    const inactiveDates = [1,2,3,4,5,6,7,8,9,10];
-    const soldOutDates = [12,14,20,26,30];
 
     return (
         <div className={`popup__container ${props.active ? '' : 'hidden'}`}>
@@ -37,13 +53,13 @@ export const PopupCalendar = (props) => {
                         {dates.map((date) => <div
                             key={date}
                             className={`calendar__date 
-                            ${inactiveDates.includes(date) ? 'disabled' : ''} 
-                            ${soldOutDates.includes(date) ? 'sold' : ''}
+                            ${inactiveDates().includes(date) && !soldOutDates().includes(date)  ? 'disabled' : ''} 
+                            ${soldOutDates().includes(date) ? 'sold' : ''}
                             `}
                             onClick={(event) => {
                                 props.setDate(`August ${event.currentTarget.textContent},2020`);
                                 props.setActive(false);
-                            }}>{soldOutDates.includes(date) ? 'sold out' : date}
+                            }}>{soldOutDates().includes(date) ? 'sold out' : date}
                         </div> )}
                     </div>
                 </div>
